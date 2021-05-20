147d629a1cde24d0659d77f62cd1aa0e9785e23c
humbertoguerrer <humberto.guerrer@gmail.com>
2021-05-19 23:04:33 -0300

    Projeto criado

---
+++ src/test/java/com/hgn/cadastropessoasapi/CadastroPessoasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hgn.cadastropessoasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroPessoasApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
